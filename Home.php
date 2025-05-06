022b081dd06fd11beb9727e255bda8f186a9ca2e
HeartV20 <velosoheart328@gmail.com>
2025-05-06 14:41:37 +0800

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,58 @@
+<html>
+    <title>NERV: User Registration</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="" rel="stylesheet">
+    <link rel="stylesheet" href="./styles.css">
+    <head>
+    </head>
+
+    <body>
+        <section class="loginSec">
+            <script type="text/javascript" src="./signUp.js"></script>
+            <div class="overlay">
+                <div class="vidBG">
+                    <video autoplay muted loop id="myVideo">
+                        <source src="./Evangelion-UI.mp4" type="video/mp4">
+                            Your browser does not support the video tag.
+                    </video>
+                </div>
+            </div>
+                    <div class="overlay">
+            <div class="suisei">
+                <table>
+                    <tr>
+                        <td class="Main1">
+                            <div class="zoom-container">
+                               <a href="file:///C:/Users/ABC/Documents/HV/xxxx.html">
+                                <img class="Logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_x_E1xggVgCVsoQu8UpcQty6u5r8qNBhdpA&s">
+                              </a>
+                            </div>
+                          </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <article class="logInfo">
+                                <fieldset class="mainProf">
+                                    <legend>
+                                        <h1><header class="profname">Log In/Sign Up</header></h1>
+                                    </legend>
+                                    <a href="https:/HeartV20.github.io/home">
+    <button type="button">Go to Home</button>
+</a>
+
+<!-- Log Out (destroy session on same page with ?logout=true) -->
+<a href="./logout.php">
+    <button type="button">Log Out</button>
+</a>
+                                </fieldset>
+                            </article>
+                        </td>
+                    </tr>
+                </table>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
